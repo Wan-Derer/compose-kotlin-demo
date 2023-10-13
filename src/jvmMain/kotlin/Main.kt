f59838ab1151003d53d924208d0b3aa1844bcdc1
-- <-->
2023-10-13 07:27:45 +0300

    UI update

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,7 +1,17 @@
-import androidx.compose.material.MaterialTheme
+/*
+https://github.com/JetBrains/compose-multiplatform-desktop-template
+https://www.goodwin.dev/dev/2022/3/9/how-to-create-a-desktop-app-using-kotlin-and-compose-desktop/
+https://androidexample365.com/a-full-stack-desktop-app-with-desktop-compose-and-spring-boot/
+
+Сборка:  ./gradlew createDistributable
+ */
+
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,19 +23,44 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+  var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+  MaterialTheme {
+    Scaffold(
+      topBar = { MyTopAppBar() }
+    ) {
+      Button(onClick = {
+        text = "Hello, Desktop!"
+      }) {
+        Text(text)
+      }
     }
+  }
+}
+
+@Composable
+fun MyTopAppBar() {
+  TopAppBar(
+    title = { Text("Simple TopAppBar") },
+    navigationIcon = {
+      IconButton(onClick = { /* doSomething() */ }) {
+        Icon(Icons.Filled.Menu, contentDescription = null)
+      }
+    },
+    actions = {
+      // RowScope here, so these icons will be placed horizontally
+      IconButton(onClick = { /* doSomething() */ }) {
+        Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+      }
+      IconButton(onClick = { /* doSomething() */ }) {
+        Icon(Icons.Filled.Add, contentDescription = "Localized description")
+      }
+    }
+  )
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
-    }
+  Window(onCloseRequest = ::exitApplication) {
+    App()
+  }
 }
